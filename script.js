c604142c5b8f6cfa3a99aa51d363b7f93beb0d62
sanaa-ahmed827 <sanaa456ahmed@gmail.com>
2026-01-22 11:29:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12= document.getElementById("btn12");
+let btn13= document.getElementById("btn13");
+let btn14= document.getElementById("btn14");
+let btn15= document.getElementById("btn15");
+let btn16= document.getElementById("btn16");
+let btn17= document.getElementById("btn17");
+
+
+let m1 = new Audio('./music/m1.mp3');
+let m2 = new Audio('./music/m2.mp3');
+let m3 = new Audio('./music/m3.mp3');
+let m4 = new Audio('./music/m4.mp3');
+let m5 = new Audio('./music/m5.mp3');
+let m6 = new Audio('./music/m6.mp3');
+let m7 = new Audio('./music/m7.mp3');
+let m8 = new Audio('./music/m8.mp3');
+let m9 = new Audio('./music/m9.wav');
+let m10 = new Audio('./music/m10.wav');
+let m11= new Audio('./music/m11.wav');
+let m12 = new Audio('./music/m12.wav');
+let m13 = new Audio('./music/m13.wav');
+let m14 = new Audio('./music/m14.mp3');
+let m15 = new Audio('./music/m15.mp3');
+let m16 = new Audio('./music/m16.mp3');
+let m17 = new Audio('./music/m17.mp3');
+
+
+function onebtn(){
+   m1.play();
+}
+
+function twobtn(){
+  m2.play();
+}
+
+function threebtn(){
+   m3.play()
+}
+
+function fourbtn(){
+   m4.play();
+}
+
+function fivebtn(){
+   m5.play();
+}
+
+function sixbtn(){
+  m6.play();
+}
+
+function sevenbtn(){
+   m7.play();
+}
+
+function eightbtn(){
+   m8.play();
+}
+
+
+function ninebtn(){ 
+ m9.play();
+}
+
+function tenbtn(){
+    m10.play();
+}
+
+
+function elevenbtn(){
+   m11.play();
+}
+
+function twelevebtn(){
+   m12.play();
+}
+
+function thirteenbtn(){
+   m13.play();
+}
+
+function forteenbtn(){
+  m14.play();
+}
+
+function fifteenbtn(){
+   m15.play();
+}
+
+function sixteeneenbtn(){
+   m16.play();
+}
+
+
+function seventeenbtn(){
+  m17.play();
+}
+
+ 
